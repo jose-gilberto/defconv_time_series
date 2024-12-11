feat: identified bug with gradients in lite

experiments/classification/univariate/lite_ucr_archive.py
@@ -14,9 +14,11 @@ import time
 import pandas as pd
 from torch.nn import functional as F
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from torch import nn
 
 # Ensure reproducibility
-seed_everything(81, workers=True)
+# seed_everything(81, workers=True)
 
 DATASETS = [
     # "ACSF1",
@@ -104,16 +106,7 @@ DATASETS = [
     # "SemgHandGenderCh2",
     # "SemgHandMovementCh2",
     # "SemgHandSubjectCh2",
-    # "ShapeletSim",
-    # "ShapesAll",
-    # "SmallKitchenAppliances",
-    # "SmoothSubspace",
-    # "SonyAIBORobotSurface1",
-    # "SonyAIBORobotSurface2",
-    # "StarLightCurves",
-    # "Strawberry",
-    # "SwedishLeaf",
-    # "Symbols",
+    # "Shapelettrain_acc",
     # "SyntheticControl",
     # "ToeSegmentation1",
     # "ToeSegmentation2",
@@ -157,12 +150,14 @@ for dataset in DATASETS:
     
     print('Converting the dataset to torch.DataLoader...')
     train_set, test_set = to_torch_dataset(X_train, y_train, X_test, y_test)
-    train_loader, test_loader = to_torch_loader(train_dataset=train_set, test_dataset=test_set, batch_size=100)
+    train_loader, test_loader = to_torch_loader(train_dataset=train_set, test_dataset=test_set, batch_size=16)
 
     num_classes = len(np.unique(y_train))
 
     for experiment_number in range(NUMBER_OF_EXPERIMENTS):
+        # model = LITE(in_channels=1, num_classes=num_classes, sequence_length=sequence_len)
         model = LITE(in_channels=1, num_classes=num_classes, sequence_length=sequence_len)
+
 
         checkpoint = ModelCheckpoint(
             monitor='train_loss',
@@ -182,37 +177,43 @@ for dataset in DATASETS:
         )
 
         # start_time = time.time()
-        trainer.fit(model, train_dataloaders=train_loader)
-        # torch.save(model.state_dict(), './lite.pth')
-
-        # model.load_state_dict(torch.load('./lite.pth'), strict=True)
-
-        # trainer.test(model, train_loader)
-  
-        # model.eval()
-        # with torch.no_grad():
-        #     for x, y in train_loader:
-        #         x = x.to(model.device)
-        #         y = y.to(model.device)
-
-        #         preds = model(x)
-
-        #         if num_classes == 2:
-        #             preds = preds.squeeze(dim=-1)
-        #             y_pred = F.sigmoid(preds).round()
-
-        #             y_pred = y_pred.cpu().detach().numpy()
-        #             y_true = y.cpu().detach().numpy()
-        #         else:
-        #             y_pred = torch.argmax(preds, dim=1).cpu().detach().numpy()
-        #             y_true = y.cpu().detach().numpy()
-
-        #         acc = accuracy_score(y_true, y_pred)
-        #         print(acc)
-
+        # trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=test_loader)
         # end_time = time.time()
+        model.to(torch.device('cuda'))
+        model.train()
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+        criteria = nn.BCEWithLogitsLoss()
+        for epoch in range(100):
+            for x, y in train_loader:
+                x = x.to(model.device)
+                y = y.to(model.device)
 
-        results = trainer.test(model, dataloaders=train_loader)
+                preds = model(x)
+                
+                # if model.num_classes == 2:
+                #     preds = preds.squeeze(dim=-1)
+                #     y_pred = F.sigmoid(preds).round()
+
+                #     y_pred = y_pred.cpu().detach().numpy()
+                #     y_true = y.cpu().detach().numpy()
+                # else:
+                #     y_pred = torch.argmax(preds, dim=1).cpu().detach().numpy()
+                #     y_true = y.cpu().detach().numpy()
+
+                loss = criteria(preds.squeeze(dim=-1), y.float().to(model.device) if model.num_classes == 2 else y)
+                # acc = accuracy_score(y_true, y_pred)
+
+                print(f'Epoch {epoch} - Train Loss: {loss}')
+
+                optimizer.step()
+                loss.backward()
+
+
+        # results = trainer.test(model, dataloaders=train_loader)
+
+        # # plt.plot(list(range(NUMBER_OF_EPOCHS)), model.train_losses)
+        # # plt.plot(list(range(NUMBER_OF_EPOCHS + 1)), model.validation_losses)
+        # # plt.show()
 
         # results_data_dir['dataset'].append(dataset)
         # results_data_dir['model'].append('lite')

src/models/lite.py
@@ -32,13 +32,34 @@ class DepthwiseSeparableConvolution1d(nn.Module):
 
 class LITE(LightningModule):
 
-    def __init__(self, in_channels: int, num_classes: int, sequence_length: int, hidden_channels: int = 32) -> None:
+    def __init__(
+        self,
+        sequence_length: int,
+        num_classes: int,
+        in_channels: int,
+        hidden_channels: int = 32,
+    ) -> None:
+        """ Create an instance of a LITE [1] model.
+
+        Based on the implementation from https://github.com/MSD-IRIMAS/LITE/blob/main/classifiers/lite.py
+
+        Args:
+            sequence_length (int): length of the time series
+            num_classes (int): number of classes for the problem
+            in_channels (int): number of channels (if the series is univariate than in_channels = 1)
+            hidden_channels (int, optional): number of hidden channels to use in inception convolutions. Defaults to 32.
+
+        References:
+            [1]
+        """
         super().__init__()
 
         self.in_channels = in_channels
         self.num_classes = num_classes
         self.sequence_length = sequence_length
         self.hidden_channels = hidden_channels
+
+        # Build model
 
         custom_kernels_sizes = [2, 4, 8, 16, 32, 64]
         custom_convolutions = []
@@ -96,6 +117,8 @@ class LITE(LightningModule):
             custom_convolutions.append(custom_conv)
 
         self.custom_convolutions = nn.ModuleList(custom_convolutions)
+        self.custom_convolutions.eval()
+
         self.custom_activation = nn.ReLU()
 
         n_convs = 3
@@ -137,7 +160,6 @@ class LITE(LightningModule):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         feature_maps = []
-
         for inception_conv in self.inception_convolutions:
             x_ = inception_conv(x)
             feature_maps.append(x_)
@@ -163,11 +185,10 @@ class LITE(LightningModule):
             feature_maps = self.separable_activation(feature_maps)
             
         feature_maps = torch.mean(feature_maps, dim=-1)
-
         return self.linear(feature_maps)
     
     def configure_optimizers(self) -> any:
-        optimizer = torch.optim.Adam(self.parameters())
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-1)
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
             optimizer, mode='min', factor=0.5, patience=50, min_lr=1e-4
         )
@@ -205,6 +226,9 @@ class LITE(LightningModule):
         self.log('train_precision', precision, on_epoch=True, on_step=False)
         self.log('train_recall', recall, on_epoch=True, on_step=False)
 
+        # self.train_accs.append(acc)
+        # self.train_losses.append(loss.item())
+
         return loss
     
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
@@ -234,6 +258,10 @@ class LITE(LightningModule):
         self.log('val_f1', f1, on_epoch=True, on_step=False)
         self.log('val_precision', precision, on_epoch=True, on_step=False)
         self.log('val_recall', recall, on_epoch=True, on_step=False)
+
+        # self.validation_accs.append(acc)
+        # self.validation_losses.append(loss.item())
+
         return
 
     def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor: